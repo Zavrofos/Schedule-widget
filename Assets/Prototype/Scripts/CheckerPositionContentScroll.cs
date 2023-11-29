91ca705783e6bf9529835aa96f2b0b680ff6604c
Zavrofos <rebrin.na98@gmail.com>
2023-11-29 22:47:33 +0600

    Added CheckerPositionContentScroll

---
+++ Assets/Prototype/Scripts/CheckerPositionContentScroll.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace Prototype.Scripts
+{
+    public class CheckerPositionContentScroll
+    {
+        private RectTransform _contentRect;
+        private float _currentPositionContent;
+        public event Action PositionChanged;
+
+        public CheckerPositionContentScroll(RectTransform contentRect)
+        {
+            _contentRect = contentRect;
+            _currentPositionContent = contentRect.anchoredPosition.y;
+        }
+
+        public void UpdateScroll()
+        {
+            if (Mathf.Abs(_currentPositionContent - _contentRect.anchoredPosition.y) > 100)
+            {
+                PositionChanged?.Invoke();
+                _currentPositionContent = _contentRect.anchoredPosition.y - _contentRect.anchoredPosition.y % 100;
+            }
+        }
+    }
+}
