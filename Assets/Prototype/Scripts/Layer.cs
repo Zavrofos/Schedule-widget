7962b2826f37634fd4a3b951545d7c8aa1792330
Zavrofos <rebrin.na98@gmail.com>
2023-11-29 22:46:57 +0600

    Added Layer

---
+++ Assets/Prototype/Scripts/Layer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Prototype.Scripts
+{
+    public class Layer
+    {
+        public readonly int InitialPosition;
+        public bool IsActive;
+        public LayerWindow LayerWindow;
+        public event Action<Layer> TurnedOn;
+        public event Action<Layer> TurnedOff;
+
+        public Layer(int initialPosition)
+        {
+            InitialPosition = initialPosition;
+            IsActive = false;
+        }
+
+        public void TurnOn()
+        {
+            IsActive = true;
+            TurnedOn?.Invoke(this);
+        }
+
+        public void TurnOff()
+        {
+            IsActive = false;
+            TurnedOff?.Invoke(this);
+        }
+    }
+}
