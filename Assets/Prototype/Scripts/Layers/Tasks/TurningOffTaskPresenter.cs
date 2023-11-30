70f34588a60d77628a55532ce20679d9d7f0f323
Zavrofos <rebrin.na98@gmail.com>
2023-11-30 18:49:31 +0600

    Added TurningOnTaskPresenter and TurningOffTaskPresenter

---
+++ Assets/Prototype/Scripts/Layers/Tasks/TurningOffTaskPresenter.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace Prototype.Scripts.Layers.Tasks
+{
+    public class TurningOffTaskPresenter : IPresenter
+    {
+        private readonly Model _model;
+        private readonly Task _task;
+        private readonly View _view;
+
+        public TurningOffTaskPresenter(Model model, Task task, View view)
+        {
+            _model = model;
+            _task = task;
+            _view = view;
+        }
+
+        public void Subscribe()
+        {
+            _task.TurnedOff += OnTurnOff;
+        }
+
+        public void Unsubscribe()
+        {
+            _task.TurnedOff += OnTurnOff;
+        }
+
+        private void OnTurnOff()
+        {
+            _task.TaskWindow.TaskRectTransform.sizeDelta = new Vector2(100, 80);
+            _task.TaskWindow.TaskRectTransform.anchoredPosition = Vector2.zero;
+            _task.TaskWindow.gameObject.SetActive(false);
+            _task.TaskWindow = null;
+        }
+    }
+}
---
+++ Assets/Prototype/Scripts/Layers/Tasks/TurningOnTaskPresenter.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace Prototype.Scripts.Layers.Tasks
+{
+    public class TurningOnTaskPresenter : IPresenter
+    {
+        private readonly Model _model;
+        private readonly Task _task;
+        private readonly View _view;
+
+        public TurningOnTaskPresenter(Model model, Task task, View view)
+        {
+            _model = model;
+            _task = task;
+            _view = view;
+        }
+
+        public void Subscribe()
+        {
+            _task.TurnedOn += OnTurnOn;
+        }
+
+        public void Unsubscribe()
+        {
+            _task.TurnedOn += OnTurnOn;
+        }
+
+        private void OnTurnOn()
+        {
+            TaskWindow taskWindow = _task.ParentLayer.LayerWindow.PoolTaskWindows.GetFreeElement();
+            
+            int sizeX = _task.EndTime - _task.StartTime;
+            float positionX = _task.StartTime + (sizeX / 2);
+
+            taskWindow.TaskRectTransform.sizeDelta = new Vector2(sizeX, taskWindow.TaskRectTransform.sizeDelta.y);
+            taskWindow.TaskRectTransform.anchoredPosition = new Vector2(positionX, taskWindow.TaskRectTransform.anchoredPosition.y);
+
+            _task.TaskWindow = taskWindow;
+        }
+    }
+}
