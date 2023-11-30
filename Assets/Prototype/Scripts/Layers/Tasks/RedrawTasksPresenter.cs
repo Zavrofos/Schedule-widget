577cc6db9675dcc7c9f6a72235abfeade64a156f
Zavrofos <rebrin.na98@gmail.com>
2023-11-30 18:49:43 +0600

    Added RedrawTasksPresenter

---
+++ Assets/Prototype/Scripts/Layers/Tasks/RedrawTasksPresenter.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+namespace Prototype.Scripts.Layers.Tasks
+{
+    public class RedrawTasksPresenter : IPresenter
+    {
+        private readonly Model _model;
+        private readonly View _view;
+
+        public RedrawTasksPresenter(Model model, View view)
+        {
+            _model = model;
+            _view = view;
+        }
+        
+        public void Subscribe()
+        {
+            _model.ContentScrollPositionChangedHorizontal += OnRedrawTasks;
+        }
+
+        public void Unsubscribe()
+        {
+            _model.ContentScrollPositionChangedHorizontal += OnRedrawTasks;
+        }
+
+        private void OnRedrawTasks()
+        {
+            foreach (var includedLayer in _model.LayersModel.IncludedLayers)
+            {
+                foreach (var task in includedLayer.Tasks)
+                {
+                    float sizeTask = task.EndTime - task.StartTime;
+                    float positionTask = task.StartTime + (sizeTask / 2);
+
+                    if (positionTask > Mathf.Abs(_view.ScrollContent.anchoredPosition.x) &&
+                        positionTask < Mathf.Abs(_view.ScrollContent.anchoredPosition.x) + 2000)
+                    {
+                        if (!task.IsActive)
+                        {
+                            task.TurnOn();
+                        }
+                    }
+                    else
+                    {
+                        if (task.IsActive)
+                        {
+                            task.TurnOff();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
