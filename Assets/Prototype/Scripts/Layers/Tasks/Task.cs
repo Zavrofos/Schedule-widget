b818a1babdbe2d64b7deef6e81ef2e6442041e36
Zavrofos <rebrin.na98@gmail.com>
2023-11-30 17:45:07 +0600

    Changed Task and created prefab TaskWindow

--- Assets/Prototype/Scripts/Layers/Tasks/Task.cs
+++ Assets/Prototype/Scripts/Layers/Tasks/Task.cs
@@ -1,14 +1,35 @@
-﻿namespace Prototype.Scripts.Layers.Tasks
+﻿using System;
+
+namespace Prototype.Scripts.Layers.Tasks
 {
     public class Task
     {
         public readonly int StartTime;
         public readonly int EndTime;
+        public bool IsActive;
+        public readonly Layer ParentLayer;
+        public TaskWindow TaskWindow;
+        public event Action TurnedOn;
+        public event Action TurnedOff;
 
-        public Task(int startTime, int endTime)
+        public Task(int startTime, int endTime, Layer parentlayer)
         {
             StartTime = startTime;
             EndTime = endTime;
+            ParentLayer = parentlayer;
+            IsActive = false;
+        }
+
+        public void TurnOn()
+        {
+            IsActive = true;
+            TurnedOn?.Invoke();
+        }
+
+        public void TurnOff()
+        {
+            IsActive = false;
+            TurnedOff?.Invoke();
         }
     }
 }
