dd04d712077a203af1aebedbb95cb134754317b1
Zavrofos <rebrin.na98@gmail.com>
2023-11-30 21:47:55 +0600

    Added PartOfTimeScale

---
+++ Assets/Prototype/Scripts/TimeScaleDir/PartOfTimeScale.cs
@@ -0,0 +1,10 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Prototype.Scripts.TimeScaleDir
+{
+    public class PartOfTimeScale : MonoBehaviour
+    {
+        public TMP_Text PartOfTimeText;
+    }
+}
--- Assets/Prototype/Scripts/View.cs
+++ Assets/Prototype/Scripts/View.cs
@@ -1,5 +1,6 @@
 ﻿using Prototype.Scripts.Layers;
 using Prototype.Scripts.Layers.Tasks;
+using Prototype.Scripts.TimeScaleDir;
 using UnityEngine;
 
 namespace Prototype.Scripts
@@ -7,8 +8,12 @@ namespace Prototype.Scripts
     public class View : MonoBehaviour
     {
         public RectTransform ScrollContent;
+        public RectTransform TimeScaleContent;
+        
         public LayerWindow LayerWindowPrefab;
         public TaskWindow TaskWindowPrefab;
+        public PartOfTimeScale PartOfTimeScalePrefab;
+        
         public int InitialCountLayers;
         public int CountLayerPool;
         public int CountTaskPool;
