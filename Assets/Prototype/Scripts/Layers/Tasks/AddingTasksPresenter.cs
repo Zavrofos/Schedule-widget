2247135ec7750983318c84d2a8dba01b939e00b3
Zavrofos <rebrin.na98@gmail.com>
2023-11-30 18:49:03 +0600

    Added AddingTasksPresenter

---
+++ Assets/Prototype/Scripts/Layers/Tasks/AddingTasksPresenter.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+namespace Prototype.Scripts.Layers.Tasks
+{
+    public class AddingTasksPresenter : IPresenter
+    {
+        private readonly Model _model;
+        private readonly Layer _layer;
+        private readonly View _view;
+        private Dictionary<Task, List<IPresenter>> TasksPresenters;
+
+        public AddingTasksPresenter(Model model, Layer layer, View view)
+        {
+            _model = model;
+            _layer = layer;
+            _view = view;
+            TasksPresenters = new Dictionary<Task, List<IPresenter>>();
+        }
+
+        public void Subscribe()
+        {
+            _layer.AddedTask += OnAddTask;
+        }
+
+        public void Unsubscribe()
+        {
+            _layer.AddedTask += OnAddTask;
+        }
+
+        private void OnAddTask(int startTime, int endTime)
+        {
+            Task newTask = new Task(startTime, endTime, _layer);
+
+            List<IPresenter> presenters = new List<IPresenter>()
+            {
+                new TurningOnTaskPresenter(_model, newTask, _view),
+                new TurningOffTaskPresenter(_model, newTask, _view)
+            };
+
+            foreach (var presenter in presenters)
+            {
+                presenter.Subscribe();
+            }
+            
+            TasksPresenters.Add(newTask, presenters);
+            
+            _layer.Tasks.Add(newTask);
+        }
+    }
+}
