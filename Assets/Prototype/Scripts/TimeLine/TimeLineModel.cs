2ae00e5e796afab2f9cd428d1bd765bfe899793b
Zavrofos <rebrin.na98@gmail.com>
2023-12-03 11:15:05 +0600

    Added TimeLineModel

---
+++ Assets/Prototype/Scripts/TimeLine/TimeLineModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace Prototype.Scripts.TimeLine
+{
+    public class TimeLineModel
+    {
+        public float CurrentPositinX = 0;
+        public Vector2 InitialSize;
+        public event Action SetedRandomPosition;
+
+        public void SetRandomPosition()
+        {
+            SetedRandomPosition?.Invoke();
+        }
+    }
+}
