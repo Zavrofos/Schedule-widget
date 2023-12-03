88cb155fa95c5744eba4dda302a4a45922bc9d83
Zavrofos <rebrin.na98@gmail.com>
2023-12-03 13:33:22 +0600

    Added AvlTree

---
+++ Assets/Prototype/Scripts/avl-tree-c-sharp-master/Bitlush.AvlTree/AvlNode.cs
@@ -0,0 +1,12 @@
+﻿namespace Prototype.Scripts.avl_tree_c_sharp_master.Bitlush.AvlTree
+{
+    public class AvlNode<TKey, TValue>
+    {
+        public AvlNode<TKey, TValue> Parent;
+        public AvlNode<TKey, TValue> Left;
+        public AvlNode<TKey, TValue> Right;
+        public TKey Key;
+        public TValue Value;
+        public int Balance;
+    }
+}
