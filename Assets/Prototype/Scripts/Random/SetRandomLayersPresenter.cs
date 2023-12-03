2f1a248727b05c03154b9be5131663277a626846
Zavrofos <rebrin.na98@gmail.com>
2023-12-03 11:14:00 +0600

    Added SetRandomLayersPresenter

---
+++ Assets/Prototype/Scripts/Random/SetRandomLayersPresenter.cs
@@ -0,0 +1,40 @@
+﻿using Prototype.Scripts.Layers;
+using UnityEngine;
+
+namespace Prototype.Scripts.Random
+{
+    public class SetRandomLayersPresenter : IPresenter
+    {
+        private readonly Model _model;
+        private readonly View _view;
+
+        public SetRandomLayersPresenter(Model model, View view)
+        {
+            _model = model;
+            _view = view;
+        }
+        
+        public void Subscribe()
+        {
+            _model.RandomizeModel.SettedRandomLayers += OnSetRandomLayers;
+        }
+
+        public void Unsubscribe()
+        {
+            _model.RandomizeModel.SettedRandomLayers -= OnSetRandomLayers;
+        }
+
+        private void OnSetRandomLayers()
+        {
+            int countRandomLayers = UnityEngine.Random.Range(20, 40);
+            int countRandomTask = UnityEngine.Random.Range(10, 100);
+
+            for (int i = 0; i < countRandomLayers; i++)
+            {
+                _model.LayersModel.Addlayer();
+                Layer newLayer = _model.LayersModel.Layers[_model.LayersModel.Layers.Count - 1];
+                _model.RandomizeModel.RandomFillLayer(newLayer, countRandomTask);
+            }
+        }
+    }
+}
