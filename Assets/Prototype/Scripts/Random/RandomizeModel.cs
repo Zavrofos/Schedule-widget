2fb1c6b035714908b270743840f1b76c4f88ddee
Zavrofos <rebrin.na98@gmail.com>
2023-12-03 11:13:46 +0600

    Added RandomizeModel

---
+++ Assets/Prototype/Scripts/Random/RandomizeModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Prototype.Scripts.Layers;
+
+namespace Prototype.Scripts.Random
+{
+    public class RandomizeModel
+    {
+        public event Action<Layer, int> RandomlyFilledTheLayer;
+        public event Action SettedRandomLayers;
+        
+        public void RandomFillLayer(Layer layer, int countTask)
+        {
+            RandomlyFilledTheLayer?.Invoke(layer, countTask);
+        }
+
+        public void SetRandomLayers()
+        {
+            SettedRandomLayers?.Invoke();
+        }
+    }
+}
