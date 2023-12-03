c432675f8379f55e5c8a3eb7dad56645670a911b
Zavrofos <rebrin.na98@gmail.com>
2023-12-03 11:18:30 +0600

    Added AddingPartOfTimeScalePresenter

---
+++ Assets/Prototype/Scripts/TimeScaleDir/AddingPartOfTimeScalePresenter.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Prototype.Scripts.TimeScaleDir
+{
+    public class AddingPartOfTimeScalePresenter : IPresenter
+    {
+        private readonly Model _model;
+        private readonly View _view;
+
+        public AddingPartOfTimeScalePresenter(Model model, View view)
+        {
+            _model = model;
+            _view = view;
+        }
+        
+        public void Subscribe()
+        {
+            _model.TimeScaleModel.AddedpartOfTimeScale += OnAddPartOfTimeScale;
+        }
+
+        public void Unsubscribe()
+        {
+            _model.TimeScaleModel.AddedpartOfTimeScale += OnAddPartOfTimeScale;
+        }
+
+        private void OnAddPartOfTimeScale()
+        {
+            PartOfTimeScale partOfTimeScale =
+                GameObject.Instantiate(_view.PartOfTimeScalePrefab, _view.TimeScaleContent);
+            partOfTimeScale.PartOfTimeText.text = (_model.TimeScaleModel.LastValueTime * 10).ToString();
+            _model.TimeScaleModel.LastValueTime++;
+            _model.TimeScaleModel.PartsOfTimeScale.Add(partOfTimeScale);
+        }
+    }
+}
