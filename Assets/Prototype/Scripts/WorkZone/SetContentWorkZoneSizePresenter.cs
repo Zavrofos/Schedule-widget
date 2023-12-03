f4cabf9f339bf32b54b9c2950d6cc1ae1c941a04
Zavrofos <rebrin.na98@gmail.com>
2023-12-03 11:19:03 +0600

    Added SetContentWorkZoneSizePresenter

---
+++ Assets/Prototype/Scripts/WorkZone/SetContentWorkZoneSizePresenter.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace Prototype.Scripts.WorkZone
+{
+    public class SetContentWorkZoneSizePresenter : IPresenter
+    {
+        private readonly Model _model;
+        private readonly View _view;
+
+        public SetContentWorkZoneSizePresenter(Model model, View view)
+        {
+            _model = model;
+            _view = view;
+        }
+
+        public void Subscribe()
+        {
+            _model.WorkZoneModel.ChangedContentSizeX += OnChangeContentSizeX;
+            _model.WorkZoneModel.ChangedContentSizeY += OnChangeContentSizeY;
+        }
+
+        public void Unsubscribe()
+        {
+            _model.WorkZoneModel.ChangedContentSizeX -= OnChangeContentSizeX;
+            _model.WorkZoneModel.ChangedContentSizeY -= OnChangeContentSizeY;
+        }
+        
+        private void OnChangeContentSizeX(float newSizeX)
+        {
+            _view.WorkZoneScrollContent.sizeDelta = new Vector2(newSizeX, _view.WorkZoneScrollContent.sizeDelta.y);
+            _model.WorkZoneModel.contentSizeX = newSizeX;
+        }
+        
+        private void OnChangeContentSizeY(float newSizeY)
+        {
+            _view.WorkZoneScrollContent.sizeDelta = new Vector2(_view.WorkZoneScrollContent.sizeDelta.x, newSizeY);
+            _model.WorkZoneModel.contentSizeY = newSizeY;
+        }
+    }
+}
